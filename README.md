65340d74391ae8adb3aea4f0998bbf4a82f0a2d5
Kilimba <dyaul003@umn.edu>
2022-06-05 17:50:15 -0500

    got my changes to save in local storage but now they won't stay on my page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Calendar
 
-##Description
+Description
 
 
 For this project, I was tasked with creating a calendar that would inform the person what the current day is and also allow them to be able to edit the tasks within the calendar. My biggest challenges include not being able to get the time in my calendar to match up with the current time. Another one was properly sending my info to my local storage. 
--- script.js
+++ script.js
@@ -21,7 +21,8 @@ $('#currentDay').text(currentDay);
 var checkHour = function (tasksSpace) {
 //get current number for hours
     var currentHour = moment().hour();
-    var momentHour =  moment().hour();
+    // var momentHour =  moment().hour(possibleHours[i]);
+    var momentHour = possibleHours [i];
     console.log(momentHour, currentHour);
     
 
@@ -38,9 +39,7 @@ var checkHour = function (tasksSpace) {
          $(tasksSpace).addClass('future');
          $(tasksSpace).removeClass("past present");
         }
-
-    
-    }
+}
 
 // Generate the tasks using js
     for (var i = 0; i < possibleHours.length; i++) {
@@ -48,26 +47,39 @@ var checkHour = function (tasksSpace) {
     // Made  a div to hold the btn and user input 
     var tasksRow = $('<div>')
         .addClass('row time-block')
+        .attr({
+            id: i + 9
+        })
         
     // add 1 div with class hour
     var tasksHour = $('<div>')
         .addClass('col-1 hour')
         .text(possibleHours[i])
+        .attr({
+            id: i + 9
+        })
         
     // add 1 div with class
     var tasksSpace = $('<div>')
         .addClass('col-10')
+        .attr({
+            id: i + 9
+        })
     
     // add p element with class
     var userInput = $('<p>')
         .addClass('description')
         .text(' ')
+        .attr({
+            id: i + 9
+        })
         
     // Save button with class 
     var saveBtn = $('<button>')
         .addClass('col-1 saveBtn')
         .attr({
             type: 'button',
+            id: i + 9
         })
 
 // Append all the element so they show up within the container div 
@@ -78,20 +90,26 @@ var checkHour = function (tasksSpace) {
     $(tasksRow).append(saveBtn);
         
     checkHour(tasksSpace);
-    
-$('.saveBtn').on('click', function () {
+}
+
+//Assign saveBtn click listener for user input and time stamp??
+$(".saveBtn").on("click", function () {
     //get nearby values.
-    var time = $(this).siblings().first().text();
+    console.log(this);
     var text = $(this).siblings().last().text();
-    console.log(time, text);
+    var hour = $(this).siblings().first().text();
     
-
+    
     //set items in local storage.
-    localStorage.setItem(time, text);
-});
+    localStorage.setItem(hour, text);
 
 
+    //load any saved data from LocalStorage
+$(".description").val(localStorage.getItem('#' + (i + 9)));
+})
 
+
+// Highlights the area where you can edit. For now, I can only click on the very top of it to display
 $('.col-10').on('click', 'p', function () {
     var text = $(this)
     .text()
@@ -104,10 +122,20 @@ var textInput = $('<textarea>')
 $(this).replaceWith(textInput);
 
 textInput.trigger('focus');
-});
+})
 
-}
-//Load tasks for first time
+// editable field was un-focused
+$('.col-10').on('blur', 'textarea', function () {
+    var text = $(this)
+        .val()
+        .trim();
+
+    var userText = $("<p>")
+        .addClass("description")
+        .text(text);
+
+    $(this).replaceWith(userText);
+});
 
 
 
